07c3b7228858ce517e38c4a820ba9709ad00b42a
tanishrajkumar <tanishraj.k@bsfdv.com>
2023-12-30 01:13:46 +0400

    feat: Episodes + Assignment

--- Episode 11 - Data is the new oil/Bhojan-food-delivery-app/src/components/Header.js
+++ Episode 11 - Data is the new oil/Bhojan-food-delivery-app/src/components/Header.js
@@ -1,9 +1,13 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { OnlineStatus } from "./OnlineStatus";
+import { ThemeContext } from "../context/ThemeContext";
 
 export const Header = () => {
+  const { theme, setTheme } = useContext(ThemeContext);
+
   return (
-    <div className=" fixed top-0 bg-grey950 shadow-sm min-h-20 min-w-full z-10 flex items-center justify-between">
+    <div className=" fixed top-0 bg-grey950 shadow-sm min-h-20 min-w-full z-50 flex items-center justify-between">
       <nav className="flex items-center justify-between max-w-screen-xl w-full m-auto px-4">
         <div className="flex items-center relative mt-2.5">
           <h1 className="text-primary text-450 font-bold">BHOJAN</h1>
@@ -34,6 +38,14 @@ export const Header = () => {
             >
               Contact
             </Link>
+          </li>
+          <li>
+            <button
+              onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+              className="filter-button text-250 font-medium text-white cursor-pointer transition-all duration-500 ease-in-ou hover:text-white"
+            >
+              {theme === "light" ? "☀︎" : "⛅️"}
+            </button>
           </li>
         </ul>
       </nav>
