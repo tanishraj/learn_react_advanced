5ce89ba11684e2210b643ed8913e61ca7091f27f
tanishrajkumar <tanishraj.k@bsfdv.com>
2024-01-31 04:37:17 +0400

    fix: search results page

--- Episode 14 - Final Food App/Bhojan-food-delivery-app/src/hooks/useSearchResult.js
+++ Episode 14 - Final Food App/Bhojan-food-delivery-app/src/hooks/useSearchResult.js
@@ -15,7 +15,7 @@ export const useSearchResult = () => {
     const searchResultsData = await fetch(
       `${SUBMIT_ACTION_SUGGESTION_API}${searchParam}${SUBMIT_ACTION_SUGGESTION_API_FRACTION}`
     );
-    searchResultsResponse = await searchResultsData.json();
+    const searchResultsResponse = await searchResultsData.json();
     const _searchResultTypeTabsData =
       searchResultsResponse?.data?.cards?.[0]?.card?.card?.tab;
     const searchResultsForDishes =
